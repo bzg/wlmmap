4bd4373f6a67d96aa0e19e1d349461479abe9b39
Bastien Guerry <bzg@altern.org>
2013-08-22 19:13:14 +0200

    Take 1000 mons.

--- src/wlmmap/handler.clj
+++ src/wlmmap/handler.clj
@@ -40,7 +40,7 @@
 (defn- generate-markers
   "return a string with the javascript code to generate addressPoints."
   [params]
-  (let [mons (take 100 (wcar* (car/smembers "frfr")))
+  (let [mons (take 1000 (wcar* (car/smembers "frfr")))
         all (remove #(nil? %)
                     (for [m mons]
                       (let [res (read-string (wcar* (car/get m)))
