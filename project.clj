4a83dffe01de6e39c61e5d881c23c2e526432877
Bastien Guerry <bzg@altern.org>
2013-09-02 21:24:01 +0200

    Amend previous commit.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject
-  wlmmap "0.0.2"
+  wlmmap "0.0.3"
   :url "http://github.com/bzg/wlmmap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
