b3f1b514228faf468f8288174ed1b89148fc620c
Bastien Guerry <bzg@altern.org>
2013-09-14 17:20:46 +0200

    Use addLayers when displaying from toolserver.

--- src/cljs/map.cljs
+++ src/cljs/map.cljs
@@ -53,25 +53,21 @@
      #(.setView mymap (vector (first %) (last %)) 5))))
 
 (defn addmarkers-toolserver [map-bounds-string]
-  (set! stopper "go")
   (let [ch (chan)
-        markers (L/MarkerClusterGroup.
-                 {:disableClusteringAtZoom (+ zoomlimit 3)})
-        per (dom/by-id "per")]
+        markers (L/MarkerClusterGroup.)]
     (set! layers (conj layers markers))
-    (go (while (not= stopper "stop")
-          (let [[[id [lat lng] img title] cnt perc] (<! ch)
-                icon ((get-in L [:mapbox :marker :icon])
-                      {:marker-symbol ""
-                       :marker-color (if img "FF0000" "0044FF")})
-                marker (-> L (.marker (L/LatLng. lat lng) {:icon icon}))]
-            (set! (.-value per) (str (Math/round perc) "%"))
-            (.bindPopup marker title)
-            (.addLayer markers marker))))
-    (remote-callback :get-markers-toolserver [map-bounds-string]
-                     #(go (doseq [[a cnt] (map list % (range 100000))]
-                            (<! (timeout 5))
-                            (>! ch [a cnt (/ (* cnt 100) (count %))]))))
+    (macros/rpc
+     (get-markers-toolserver map-bounds-string) [res]
+     (.addLayers
+      markers
+      (into [] (map #(let [[id [lat lng] img title] %
+                           icon ((get-in L [:mapbox :marker :icon])
+                                 {:marker-symbol ""
+                                  :marker-color (if img "FF0000" "0044FF")})
+                           marker (-> L (.marker (L/LatLng. lat lng) {:icon icon}))]
+                       (.bindPopup marker title)
+                       marker)
+                    res))))
     (.addLayer mymap markers)))
 
 (defn- geolocalize [position]
@@ -88,7 +84,15 @@
     (when (< z zoomlimit)
       (set! (.-innerHTML sh) "...."))))
 
-(.on mymap "zoomend" #(maybe-show-here))
+(.on mymap "zoomend" maybe-show-here)
+
+(defn- addmarkers-plus []
+  (when (or (>= (.getZoom mymap) zoomlimit)
+            (js/confirm "Possibly display up to 5000 monuments?"))
+    (when (seq layers) (removelastlayer))
+    (addmarkers-toolserver (.toBBoxString (.getBounds mymap)))))
+
+(.on mymap "dragend" addmarkers-plus)
 
 (defn- setmap [local]
   (let [db (dom/by-id "db")
@@ -105,15 +109,17 @@
     (set! (.-onclick stop) #(set! stopper "stop"))
     (set! (.-onclick sh)
           #(let [z (.getZoom mymap)]
-             (when (and (< z zoomlimit)
-                        (js/confirm
-                         (str "This will load up to 5000 monuments from the database.\n\n"
-                              "It takes a while if the focus is too large.\n\n"
-                              "Zoom " (- zoomlimit z)
-                              " times to be more comfortable.")))
-               (do (set! stopper "stop")
-                   (when (seq layers) (removelastlayer))
-                   (addmarkers-toolserver (.toBBoxString (.getBounds mymap)))))))))
+             (when (or (>= z zoomlimit)
+                       (and (< z zoomlimit)
+                            (js/confirm
+                             (str "This will load up to 5000 monuments from the database.\n\n"
+                                  "It takes a while if the focus is too large.\n\n"
+                                  "Zoom " (- zoomlimit z)
+                                  " times to be more comfortable."))))
+               (set! stopper "stop")
+               (when (seq layers) (removelastlayer))
+               (addmarkers-toolserver (.toBBoxString (.getBounds mymap))))))))
+
 (defn- init []
   (let [lang0 (.-language js/navigator)
         loc (.-location js/window)
