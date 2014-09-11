2810eb2809261c5bb238a7e743b44528b3b52bed
Bastien Guerry <bzg@altern.org>
2014-09-11 10:32:38 +0200

    Fix port

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject
-  wlmmap "0.0.9"
+  wlmmap "0.1.0"
   :url "http://github.com/bzg/wlmmap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
