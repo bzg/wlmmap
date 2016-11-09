ff22f26f5c3766764e0d5d9cf765b2647d52c844
Bastien <bzg@gnu.org>
2016-11-09 22:10:18 +0100

    Reuse getCurrentPosition

--- src/cljs/map.cljs
+++ src/cljs/map.cljs
@@ -77,7 +77,7 @@
     ;; FIXME: first switch to https for panoramap.org
     ;; http://stackoverflow.com/questions/32106849/getcurrentposition-and-watchposition-are-deprecated-on-insecure-origins
     ;; (when (= local 1)
-    ;;   (.getCurrentPosition (.-geolocation js/navigator) geolocalize))
+    (.getCurrentPosition (.-geolocation js/navigator) geolocalize))
     (set! (.-onclick stop) #(set! stopper "stop"))
     (set! (.-onclick sh)
           #(let [z (.getZoom mymap)]
