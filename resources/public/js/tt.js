9f54876184a49496fcead848e81ef9688bb663ce
Bastien <bzg@gnu.org>
2016-11-15 23:26:53 +0100

    Clean up.

--- resources/public/js/tt.js
+++ resources/public/js/tt.js
@@ -1,4 +1,4 @@
 current_url = window.location.href;
 current_title = encodeURIComponent(document.title);
-tweet = "<a title=\"Share on twitter\" target=\"_blank\" href=\"https://twitter.com/share?via=bzg2&text=" + current_title + " http://www.panoramap.org\"\">Tweet :)</a>";
+tweet = "<a title=\"Share on twitter\" target=\"_blank\" href=\"https://twitter.com/share?via=bzg2&text=" + current_title + " http://www.panoramap.org\"\">Tweet this!</a>";
 document.write(tweet);
